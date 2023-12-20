757cef5dffcfe931672e06970db603edd0d34bf6
Bradley <bradbro101@googlemail.com>
2023-12-20 00:03:44 +0000

    Removal of namespaces as research outcome on namespaces showed it was bad practice. Added interval timer to an auto scroll function. This checks whether the autoscroll checkbox is checked and acts accordingly. Added elif statement to deal with ticked or unticked box.

--- js/functionality.js
+++ js/functionality.js
@@ -1,7 +1,34 @@
-const functionality_namespace = function () {
+const carousel = function () {
+    let auto_scroll;
+    const set_auto_scroll = function () {
+        if (!auto_scroll) {
+            auto_scroll = setInterval(auto_scroll_function, 5000);
+        } else {
+            clearInterval(auto_scroll);
+            auto_scroll = null;
+        }
+    }
+    const auto_scroll_function = function () {
 
+        let enabled = document.getElementById("auto_scroll").checked;
+
+        if (enabled) {
+            scroll_left()
+        }
+
+    }
+    const scroll_left = function () {
+
+    }
+    const scroll_right = function () {
+
+    }
+
+    document.getElementById("auto_scroll").addEventListener("click", set_auto_scroll);
 
     return {
-
+        set_auto_scroll : set_auto_scroll,
+        scroll_left : scroll_left,
+        scroll_right : scroll_right
     }
 }
